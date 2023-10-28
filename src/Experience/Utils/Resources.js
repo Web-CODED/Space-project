52b045ecc51ac3f01904eae0b21ebc34a17e100d
Web-CODED <progeng94@gmail.com>
2023-10-28 09:14:30 +0530

    Add files via upload

---
+++ src/Experience/Utils/Resources.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+import EventEmitter from "./EventEmitter";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+export default class Resource extends EventEmitter {
+  constructor(sources) {
+    super();
+
+    // Options
+    this.sources = sources;
+
+    // Setup
+    this.items = {};
+    this.toLoad = this.sources.length;
+    this.loaded = 0;
+    this.loadingProgress = document.querySelector("#loadingProgress");
+
+    this.setLoaders();
+    this.startLoading();
+  }
+
+  setLoaders() {
+    this.loaders = {};
+    this.loaders.gltfLoader = new GLTFLoader();
+    this.loaders.textureLoader = new THREE.TextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+  }
+
+  startLoading() {
+    // Load each source
+    for (const source of this.sources) {
+      if (source.type === "gltfModel") {
+        this.loaders.gltfLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "texture") {
+        this.loaders.textureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "cubeTexture") {
+        this.loaders.cubeTextureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      }
+    }
+  }
+
+  sourceLoaded(source, file) {
+    this.items[source.name] = file;
+    this.loaded++;
+
+    // Get loading progress %
+    this.loadingProgress.innerHTML =
+      "Loading... " + Math.round((this.loaded / this.toLoad) * 100) + "%";
+
+    if (this.loaded === this.toLoad) {
+      // Loading is ready
+      this.trigger("ready");
+    }
+  }
+}
